509bac915fd3a02c471a1bb96839b53afdadc624
svvladimir-ru <58976702+svvladimir-ru@users.noreply.github.com>
2020-07-20 19:46:25 +0300

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -1,5 +1,7 @@
+'''Для запуска скрипта раз в неделю, нужно будет настроить планировщик задач, например cron'''
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 url = 'https://gazoncity.ru/catalog/'
@@ -8,10 +10,21 @@ HEADERS = {
     'accept': '*/*'
 }
 HOST = 'https://gazoncity.ru'
+FILE = 'gazoncity.csv'
+
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
+
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find('ul', class_='pagination').find_all('a')[-2]
+    if pagination:
+        return int(pagination.get_text())
+    else:
+        return 1
 
 
 def get_content(html):
@@ -29,12 +42,34 @@ def get_content(html):
     return product
 
 
-def parse():
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['название', 'цена', 'url-картинки', 'Описание(вес, габариты)'])
+        for item in items:
+            writer.writerow(
+                [
+                    item['title'],
+                    item['price'],
+                    item['img'],
+                    item['description']
+                ]
+            )
+
+
+def parser():
     html = get_html(url)
     if html.status_code == 200:
-        product = get_content(html.text)
-        return product
+        product = []
+        pages_count = get_pages_count(html.text)
+        for page in range(1, pages_count + 1):
+            print(f'Парсинг страницы {page} из {pages_count}')
+            html = get_html(url, params={'PAGEN_1': page})
+            product.extend(get_content(html.text))
+        save_file(product, FILE)
+        print(product)
     else:
         print('Error')
 
-print(parse())
+
+parser()
