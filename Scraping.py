815302576706dd8384f69883502905b76f19be3b
svvladimir-ru <58976702+svvladimir-ru@users.noreply.github.com>
2020-07-22 11:03:56 +0300

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -31,27 +31,58 @@ def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_='col-md-12 col-sm-12 col-xs-12')
     product = []
+    url_product = []
 
-    for item in items:
-        product.append({
-            'title': item.find('div', class_='title').get_text(strip=True),
-            'price': item.find('span', class_='price_val').get_text(),
-            'img': HOST + item.find('a', class_='blink').get('href'),
-            'description': item.find('div', class_='description').get_text(strip=True).replace('Для перехода в карточку товара с подробным описанием кликните по заголовку', ''),
-        })
+    for url in items:
+        url_product.append(HOST + url.find('div', class_='title').find_next('a').get('href'))
+
+    for item in url_product:
+        weight = 0
+        height, length, width = 0, 0, 0
+        soup = BeautifulSoup(requests.get(item).text, 'html.parser')
+        product_items = soup.find_all('tr', class_='char')
+        title = soup.find(id='pagetitle').get_text()
+        price = soup.find('span', class_='price_val').get_text()
+        img = HOST + soup.find('li', class_='col-md-1 col-sm-1 item').find_next('img').get('src')
+        description = soup.find('div', class_='previewtext').get_text(strip=True)
+        for i in product_items:
+            name = i.find('td', class_='char_name').get_text(strip=True)
+            if 'Упаковка' in name:
+                weight = i.find('td', class_='char_value').get_text(strip=True) + 'кг'
+            if 'Высота' in name:
+                height = i.find('td', class_='char_value').get_text(strip=True)
+            elif 'Длина' in name:
+                length = i.find('td', class_='char_value').get_text(strip=True)
+            elif 'Ширина' in name:
+                width = i.find('td', class_='char_value').get_text(strip=True)
+
+        product.append(
+                {
+                    'title': title,
+                    'price': price,
+                    'img': img,
+                    'height': height,
+                    'length': length,
+                    'width': width,
+                    'weight': weight,
+                    'description': description,
+            }
+            )
     return product
 
 
 def save_file(items, path):
     with open(path, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
-        writer.writerow(['название', 'цена', 'url-картинки', 'Описание(вес, габариты)'])
+        writer.writerow(['название', 'цена', 'url-картинки', 'габариты', 'вес', 'Описание'])
         for item in items:
             writer.writerow(
                 [
                     item['title'],
                     item['price'],
                     item['img'],
+                    str(item['height'])+str('*')+str(item['length'])+str('*')+str(item['width']),
+                    item['weight'],
                     item['description']
                 ]
             )
